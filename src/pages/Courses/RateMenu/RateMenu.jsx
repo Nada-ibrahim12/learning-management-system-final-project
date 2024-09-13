f777d074a82a70c77c7a9acabfba30a47a422d7e
Nada-ibrahim12 <123187920+Nada-ibrahim12@users.noreply.github.com>
2024-09-13 18:34:15 +0300

    CoursesPage rateMenu

---
+++ src/pages/Courses/RateMenu/RateMenu.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+export default function RateMenu() {
+      const appearRateMenu = () => {};
+
+  return (
+      <div className="w-100">
+        <div
+          onClick={appearRateMenu}
+          className="d-flex justify-content-between w-100"
+        >
+          <p>Rating</p>
+          <i className="fa-solid fa-angle-down ps-2 align-items-center"></i>
+        </div>
+        <hr />
+        <div className="rate ms-3">
+          <div className="stars5">
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+          </div>
+          <div className="stars4">
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+          </div>
+          <div className="stars3">
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+          </div>
+          <div className="stars2">
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+          </div>
+          <div className="stars1">
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+          </div>
+        </div>
+      </div>
+  );
+}
